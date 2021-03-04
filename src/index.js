792decde356444b1815a9b82adb8ea2cdcca97bb
Jacob Lepler <lepler.jacob@gmail.com>
2021-03-04 03:26:31 -0500

    added button and now creates separate bundle files dynamically

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import _ from 'lodash';
+import printMe from './print.js'
 
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button');
 
-    element.addEventListener = _.join(["Hello", "webpack"], ' ');
+    element.innerHTML = _.join(["Hello", "webpack"], ' ');
 
+    btn.innerHTML = 'CLick me and check the console'
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
 
     return element;
 }
