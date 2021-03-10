6996267760906d102f7c3662afe6133d8988b7ac
Jacob Lepler <lepler.jacob@gmail.com>
2021-03-10 16:31:38 -0500

    started again

--- src/print.js
+++
@@ -1,7 +0,0 @@
-// function will be used in src/index.js
-
-// print.js will be added as a new entry point. An entry point is the file that will be fed into the bundle. The output bundle can be dynamically named based on entry point names
-
-export default function printMe(){
-    console.log('I get called from print.js');
-}
