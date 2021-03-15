58605ce6327392cee18cee2f1f6a8bd293cd333d
Jacob Lepler <lepler.jacob@gmail.com>
2021-03-15 14:42:30 -0400

    end guide

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,37 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
 const path = require("path");
 
-module.exports = {
-  mode: "development",
-  entry: {
-    index: "./src/index.js",
-  },
-  output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist")
-  },
+module.exports = (env) => {
+  console.log("NODE_ENV: ", env.NODE_ENV);
+  console.log("Production: ", env.production);
+
+  return {
+    mode: "development",
+    entry: "./src/index.js",
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: "Caching",
+      }),
+    ],
+    output: {
+      filename: "[name].[contenthash].js",
+      path: path.resolve(__dirname, "dist"),
+      clean: true,
+    },
+    optimization: {
+      moduleIds: "deterministic",
+      runtimeChunk: "single",
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: "vendors",
+            chunks: "all",
+          },
+        },
+      },
+    },
+  };
 };
